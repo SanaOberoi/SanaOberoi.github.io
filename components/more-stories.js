d469a45fa033b4a8514c10bc34043ef5a6077eef
SanaOberoi <sanaoberoi98@gmail.com>
2026-02-28 13:02:11 +0530

    Update more-stories.js

--- components/more-stories.js
+++ components/more-stories.js
@@ -3,16 +3,11 @@ import PostPreview from '../components/post-preview'
 export default function MoreStories({ posts }) {
   return (
     <section>
-      <h2 className="mb-4 text-3xl md:text-4xl leading-tight font-bold">
-        <a
-          href={`/projects`}
-          className="hover:text-success duration-200 transition-colors p-4 block"
-        >
-          More Projects
-        </a>
+      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+        Selected Initiatives
       </h2>
-      <p className="text-lg text-slate-500 mb-8 px-4">
-        These projects highlight select features and products I've helped bring to life. While they represent some of my favorite work, they're just a snapshot of my broader portfolio. Due to confidentiality agreements, many projects can't be shared here, but these examples demonstrate my approach to product development and problem-solving.
+      <p className="text-lg block text-slate-500 leading-relaxed max-w-3xl mb-12">
+        These projects highlight select initiatives I've helped bring to life. While they represent some of my favorite work, they're just a snapshot of my broader portfolio. These examples demonstrate my approach to product development and problem-solving.
       </p>
       <div className="mb-8">
         {posts.map((post) => (
