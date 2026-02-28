defac198ad26dfd0ff3dab6feb44055396d0bdd7
SanaOberoi <sanaoberoi98@gmail.com>
2026-02-28 13:09:56 +0530

    Update more-stories.js

--- components/more-stories.js
+++ components/more-stories.js
@@ -3,11 +3,11 @@ import PostPreview from '../components/post-preview'
 export default function MoreStories({ posts }) {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight text-left">
         Selected Initiatives
       </h2>
-      <p className="text-lg block text-slate-500 leading-relaxed max-w-3xl mb-12">
-        These projects highlight select initiatives I've helped bring to life. While they represent some of my favorite work, they're just a snapshot of my broader portfolio. These examples demonstrate my approach to product development and problem-solving.
+      <p className="text-lg block text-slate-500 leading-relaxed mb-12 text-left">
+        These projects highlight select initiatives I've led or collectively brought to life. While they represent some of my favorite work, they're just a snapshot of my broader portfolio. These examples demonstrate my approach to value creation and ecosystem building.
       </p>
       <div className="mb-8">
         {posts.map((post) => (
